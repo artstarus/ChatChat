42a4a3efb12f914e44c3c428b347ac8f3386315c
artstarus <astarus377@gmail.com>
2024-03-13 15:03:59 -0400

    Add user models, controllers, and connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    //root route http://localhost:3000/
-    res.send("Hello World!!!");
-});
-
+dotenv.config();
+app.use(express.json()); //to parse the incoming request with JSON payload (from req.body)
 app.use("/api/auth", authRoutes);
 
+// pp.get("/", (req, res) => {
+     //root route http://localhost:3000/
+//    res.send("Hello World.");
+// });
 
-app.listen(PORT, () => console.log('Server Running on port $(PORT)'));
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log('Server Running on port $(PORT)')
+});
