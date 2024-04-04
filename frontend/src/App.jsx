5ccb414b3c61ba0dd9adc206e12d72475337c834
artstarus <astarus377@gmail.com>
2024-04-04 02:18:32 -0400

    Complete ui design (login, signup, home)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './App.css';
 import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+import Home from './pages/home/Home';
 
 function App() {
 
   return <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Home/>
     </div>;
 }
 
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-            <h1 className='text-3x1 font-semibold text-center text-gray-300'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
                 Login
                 <span className='text-blue-500'> ChatChat</span>
             </h1>
@@ -27,7 +27,7 @@ const Login = () => {
                         className='w-full input input-bordered h-10'
                     />
                 </div>
-                
+
                 <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
                     {"Don't"} have an account?
                 </a>
@@ -38,7 +38,7 @@ const Login = () => {
             </form>
         </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
