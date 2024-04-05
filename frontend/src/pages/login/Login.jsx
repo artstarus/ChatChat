48492b9b48422b1443de4fb97af0c90cb5d0c548
artstarus <astarus377@gmail.com>
2024-04-05 02:32:00 -0400

    Implement login and logout functionality

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import useLogin from '../../hooks/useLogin';
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {loading, login} = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password)
+  }
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -10,12 +23,15 @@ const Login = () => {
                 <span className='text-blue-500'> ChatChat</span>
             </h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label className='label p-2'>
                         <span className='text-base label-text'>Username</span>
                     </label>
-                    <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+                    <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </div>
 
                 <div>
@@ -26,6 +42,8 @@ const Login = () => {
                         type='password'
                         placeholder='Enter password'
                         className='w-full input input-bordered h-10'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
 
@@ -34,7 +52,10 @@ const Login = () => {
                 </Link>
 
                 <div>
-                    <button className='btn btn-block btn-sm mt-2'>Login</button>
+                    <button className='btn btn-block btn-sm mt-2'
+                        disabled = {loading}>
+                    {loading ? <span className='loading loading-spinner'></span> : "Login"}
+                    </button>
                 </div>
             </form>
         </div>
