9c2a426bc031f2bca4fec675be2dc60841feec90
artstarus <astarus377@gmail.com>
2024-04-07 23:08:02 -0400

    Implement messaging and search functionality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import Message from './Message';
+import useGetMessage from '../../hooks/useGetMessage';
+import MessageSkeleton from '../skeletons/MessageSkeleton';
 
 const Messages = () => {
+  const {messages, loading} = useGetMessage();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({behavior: "smooth"});
+    },100)
+  }, [messages]);
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+      {!loading && messages.length > 0 && messages.map((message) => <div key={message._id} ref={lastMessageRef}> <Message message={message}/> </div> )}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx}/>)}
+      {!loading && messages.length === 0 && <p className='text-center'>Send a message to start the conversation</p>}
     </div>
   );
 };
