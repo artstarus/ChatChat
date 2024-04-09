494a9a12b7add318cbbe69fc725ca996c01325ce
artstarus <astarus377@gmail.com>
2024-04-08 23:17:58 -0400

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -11,6 +12,8 @@ import {app, server} from "./socket/socket.js";
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming request with JSON payload (from req.body)
@@ -20,10 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// pp.get("/", (req, res) => {
-     //root route http://localhost:3000/
-//    res.send("Hello World.");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 server.listen(PORT, () => {
